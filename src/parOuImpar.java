128b40a3d42231f613ae93d13920ca94256f8499
Arthur201 <arthurlaureanosilva2001@gmail.com>
2022-10-26 18:16:52 -0300

    Criei alguns métodos para "enxugar" o código do programa ParOuÍmpar

--- src/parOuImpar.java
+++ src/parOuImpar.java
@@ -9,21 +9,44 @@ public class parOuImpar {
         Scanner scan2 = new Scanner(System.in);
         String querContinuar = "S";
         try {
-            while (querContinuar.equals("S")) {
+            while (querAveriguar(querContinuar)) {
                 System.out.print("Digite o número a ser averiguado:");
                 numero = scan.nextInt();
                 System.out.println((numero % 2 == 0) ? "O numero é par" : "O numero é impar"); // operador Ternário
-                System.out.println("Você deseja averiguar outro número? Digite S para sim ou N para não.");
-                querContinuar = scan2.nextLine();
-                while (!querContinuar.equals("S") && !querContinuar.equals("N")){
-                    System.out.println("Não entendemos sua solicitação. Por favor, responda novamente:");
-                    System.out.println("Você deseja averiguar outro número? Digite S para sim ou N para não.");
-                    querContinuar = scan2.nextLine();
-                }
+                perguntaAveriguacao();
+                querAveriguar(querContinuar);
             }
-        }catch(InputMismatchException e){
+        } catch (InputMismatchException e) {
             System.out.println("O número que você digitou não é um número inteiro. Tente novamente.");
             main(null);
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+            querAveriguar(querContinuar);
+        }
+    }
+    public static void respostaValida(String resposta){
+        if(!resposta.equals("S") && !resposta.equals("N")){
+            System.out.println("Não entendemos sua solicitação. Por favor, digite novamente.");
+            perguntaAveriguacao();
+        }
+    }
+
+    public static boolean querAveriguar(String resposta) {
+        if (resposta.equals("S")) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    public static void perguntaAveriguacao() {
+        System.out.println("Você deseja averiguar outro número? Digite S para sim ou N para não.");
+        Scanner scan2 = new Scanner(System.in);
+        String querContinuar = scan2.nextLine();
+        respostaValida(querContinuar);
+        if(querContinuar.equals("N")){
+            System.exit(0);
         }
     }
 
